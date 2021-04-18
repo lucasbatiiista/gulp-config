7966ba75235c8efac6edaa2fdf8b9f5b16ac9d6b
Lucas Batista <lucasbatista@outlook.com>
2021-04-18 00:52:15 -0300

    php inside server was add at gulp, replacing wamp

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,10 @@ const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const php = require('gulp-connect-php');
 const browserSync = require('browser-sync').create();
 
 // PATHS
-const pathWampLocalhost = 'personal/gulp-config/'
-
 const pathSourceSass = './src/scss/'
 const pathDestCss = './build/css/'
 
@@ -17,9 +16,22 @@ const pathDestJs = './build/js/'
 
 // BROWSER SYNC
 task('browserSync', () => {
-  browserSync.init({
-    proxy: pathWampLocalhost
+
+  // PS.You need to activate your PHP Path
+  php.server({
+    base: './',
+    port: 3000,
+    keepalive: true,
+    // custom PHP locations
+    bin: 'C:/wamp64/bin/php/php7.4.0/php.exe',
+    ini: 'C:/wamp64/bin/php/php7.4.0/php.ini',
   });
+  browserSync.init({
+    proxy: "localhost:3000",
+    baseDir: "./",
+    notify: false,
+  });
+
 });
 
 // TASKS
